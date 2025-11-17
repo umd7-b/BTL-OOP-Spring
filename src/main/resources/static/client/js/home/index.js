1cc83f49290729efde727616bb3fda5270fbd557
Pham Anh Tu <tuanhpham1751@gmail.com>
2025-11-17 13:19:38 +0700

    banner

--- src/main/resources/static/client/js/home/index.js
+++ src/main/resources/static/client/js/home/index.js
@@ -252,3 +252,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 function viewProduct(id) {
     window.location.href = `/sanpham/${id}`;
 }
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
